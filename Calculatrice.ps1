9423cd26cb090ae5ae1fec19a6be85159480ff14
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-05-14 09:18:45 +0200

    ajout de tous les nombres et le graphisme de la calculatrice

--- Calculatrice.ps1
+++ Calculatrice.ps1
@@ -23,12 +23,7 @@ $MainForm.StartPosition = " CenterScreen "
 $MainForm.Width = 500
 $MainForm.Height = 500
 
-<#
 
-# Demande à l'utilisateur les valeurs
-[uint16]$premierNombre =  Read-Host -Prompt "Entrez votre premier chiffre";
-[uint16]$deuxiemeNombre =  Read-Host -Prompt "Entrez votre deuxième chiffre";
-[string]$operateur = Read-Host -Prompt "Entrez votre opérateur (+, -, /, *)";
 
 # Calculer le résultat en fonction de l'opérateur
 Switch -Exact($operateur)
@@ -57,9 +52,9 @@ Switch -Exact($operateur)
     default  
     {
         Write-Host "Erreur";
-        Clear-Variable -Name "Resultat";
     }
 }
+
 
 # Ecrit le résultat
 if($Resultat)
@@ -67,26 +62,167 @@ if($Resultat)
     Write-Host "Résultats : $premierNombre $operateur $deuxiemeNombre = $Resultat";
 }
 
-#>
+
+$zero = Windows.FindName("$btnzero")
+$un = Windows.FindName("$btnun")
+$deux = Windows.FindName("$btndeux")
+$trois = Windows.FindName("$btntrois")
+$quatre = Windows.FindName("$btnquatre")
+$cinq = Windows.FindName("$btncinq")
+$six = Windows.FindName("$btnsix")
+$sept = Windows.FindName("$btnsept")
+$huit = Windows.FindName("$btnhuit")
+$neuf = Windows.FindName("$btnneuf")
+
+
+$zero.Add_Click({
+        $textbox.Text = $textbox.Text + '0'
+        $MainForm.Text = "cool"
+    })
+
+$btnun.Add_Click({
+        $textbox.Text = $textbox.Text + '1';
+    })
+
+$btndeux.Add_Click({
+        $textbox.Text = $textbox.Text + '2';
+    })
+
+$btntrois.Add_Click({
+        $textbox.Text = $textbox.Text + '3';
+    })
+
+$btnquatre.Add_Click({
+        $textbox.Text = $textbox.Text + '4';
+    })
+
+$btncinq.Add_Click({
+        $textbox.Text = $textbox.Text + '5';
+    })
+
+$btnsix.Add_Click({
+        $textbox.Text = $textbox.Text + '6';
+    })
+
+$btnsept.Add_Click({
+        $textbox.Text = $textbox.Text + '7';
+    })
+
+$btnhuit.Add_Click({
+        $textbox.Text = $textbox.Text + '8';
+    })
+
+$btnneuf.Add_Click({
+        $textbox.Text = $textbox.Text + '9';
+    })
 
 
 
+<# Création du bouton 0 #>
+$zero = New-Object System.Windows.Forms.Button
+
+$zero.Text = " 0 "
+
+$zero.Location = ‘140, 160’
+
+$zero.Click = $zero
+
+$MainForm.Controls.Add($zero)
 
 
 
+<# Création du bouton 1 #>
+$un = New-Object System.Windows.Forms.Button
+
+$un.Text = " 1 "
+
+$un.Location = ‘60,70’
+
+$MainForm.Controls.Add($un)
 
 
+<# Création du bouton 2 #>
+$deux = New-Object System.Windows.Forms.Button
+
+$deux.Text = " 2 "
+
+$deux.Location = ‘140,70’
+
+$MainForm.Controls.Add($deux)
 
 
+<# Création du bouton 3 #>
+$trois = New-Object System.Windows.Forms.Button
+
+$trois.Text = " 3 "
+
+$trois.Location = ‘220,70’
+
+$MainForm.Controls.Add($trois)
 
 
+<# Création du bouton 4 #>
+$quatre = New-Object System.Windows.Forms.Button
+
+$quatre.Text = " 4 "
+
+$quatre.Location = ‘60, 100’
+
+$MainForm.Controls.Add($quatre)
 
 
+<# Création du bouton 5 #>
+$cinq = New-Object System.Windows.Forms.Button
 
-$x = 60
-$y = 100
+$cinq.Text = " 5 "
+
+$cinq.Location = ‘140,100’
+
+$MainForm.Controls.Add($cinq)
+
+
+<# Création du bouton 6 #>
+$six = New-Object System.Windows.Forms.Button
+
+$six.Text = " 6 "
+
+$six.Location = ‘220, 100’
+
+$MainForm.Controls.Add($six)
+
+
+<# Création du bouton 7 #>
+$sept = New-Object System.Windows.Forms.Button
+
+$sept.Text = " 7 "
+
+$sept.Location = ‘60, 130’
+
+$MainForm.Controls.Add($sept)
+
+
+<# Création du bouton 8 #>
+$huit = New-Object System.Windows.Forms.Button
+
+$huit.Text = " 8 "
+
+$huit.Location = ‘140, 130’
+
+$MainForm.Controls.Add($huit)
+
+
+<# Création du bouton 9 #>
+$neuf = New-Object System.Windows.Forms.Button
+
+$neuf.Text = " 9 "
+
+$neuf.Location = ‘220,130’
+
+$MainForm.Controls.Add($neuf)
+ 
 
 <# Création de bouton pour les nombres #>
+<#
 for ($i = 0; $i -ge 9; $i++) {
     $btn = New-Object System.Windows.Forms.Button
 
@@ -98,12 +234,14 @@ for ($i = 0; $i -ge 9; $i++) {
     $MainForm.Controls.Add($btn)
 }
 
+#>
+
 <# Création du bouton + #>
 $plus = New-Object System.Windows.Forms.Button
 
 $plus.Text = " + "
 
-$plus.Location = ‘180,200’
+$plus.Location = ‘180,190’
 
 $MainForm.Controls.Add($plus)
 
@@ -112,7 +250,7 @@ $MainForm.Controls.Add($plus)
 $moins = New-Object System.Windows.Forms.Button
 
 $moins.Text = " - "
-$moins.Location = ‘100,200’
+$moins.Location = ‘100,190’
 
 $MainForm.Controls.Add($moins)
 
@@ -120,7 +258,7 @@ $MainForm.Controls.Add($moins)
 $fois = New-Object System.Windows.Forms.Button
 
 $fois.Text = " * "
-$fois.Location = ‘180,170’
+$fois.Location = ‘180,220’
 
 $MainForm.Controls.Add($fois)
 
@@ -128,7 +266,7 @@ $MainForm.Controls.Add($fois)
 $diviser = New-Object System.Windows.Forms.Button
 
 $diviser.Text = " / "
-$diviser.Location = ‘100,170’
+$diviser.Location = ‘100,220’
 
 $MainForm.Controls.Add($diviser)
 
@@ -140,13 +278,14 @@ $egal.Location = ‘140,250’
 
 $MainForm.Controls.Add($egal)
 
-<# Création du bouton = #>
-$egal = New-Object System.Windows.Forms.textbox
+<# Création de la textbox #>
+$textbox = New-Object System.Windows.Forms.textbox
 
-$egal.Text = " "
-$egal.Location = ‘140,50’
+$textbox.Text = " "
+$textbox.Location = ‘130,40’
 
-$MainForm.Controls.Add($egal)
+$MainForm.Controls.Add($textbox)
+
 
 <# Affichage de la page #>
 $MainForm.ShowDialog()
