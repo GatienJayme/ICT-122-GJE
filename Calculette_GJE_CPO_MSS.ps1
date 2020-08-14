016ef89dcd25ba22d61a08cd95d4f9c13e866ada
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-08-14 13:25:10 +0200

    la calculette WIP

--- Calculette_GJE_CPO_MSS.ps1
+++ Calculette_GJE_CPO_MSS.ps1
@@ -1,18 +1,12 @@
-﻿#===========================================
+﻿#================================
 #Name : Calculette.ps1
-#Author : Christopher Pardo
-#Date : 2020.05.14
-#Version : 1.2
+#Author : Christopher Pardo / Gatien Jayme / Miguel Soares / Kevin Vaucher
+#Date : 14.05.2020
+#Update Date : 28.05.2020
+#Version : 1.5
 #Title : Calculatrice
-#Description : Ce programme permettra fonctionnera comme une calculs
-#===========================================
-#Author : Gatien Jayme
-#Date : 2020.05.28
-#Version : 1.4
-#Commentaire: Rajouter le bouton reset et agrandir le texte
-#             Pouvoir continuez le calculs
-#==========================================================
-
+#Description : This programm generate a calculator
+#================================
 
 #Graphic part ======================================================================================================================================================================
 [xml]$xaml = @"
@@ -22,7 +16,8 @@
     x:Name="Window" Title="Initial Window" WindowStartupLocation = "CenterScreen"
     Width = "800" Height = "600" ShowInTaskbar = "True">
         <Canvas x:Name="Canvas">
-            <TextBox Name="TextBox" Height="75" Width="315" Canvas.Top="160" Canvas.Left="50" IsReadOnly="True" FontSize="30" TextWrapping="Wrap"></TextBox>
+            <TextBox Name="TextBox" Height="75" Width="315" Canvas.Top="80" Canvas.Left="50" IsReadOnly="True" FontSize="30" TextWrapping="Wrap"></TextBox>
+            <TextBox Name="Historic" Height="480" Width="240"  Canvas.Left="530"  Canvas.Top="75" IsReadOnly="True" FontSize="30" TextWrapping="Wrap"></TextBox>
             
             <Button x:Name = "Button0" Height = "75" Width = "75" Content = '0' ToolTip = "0"
             Canvas.Left = '50' Canvas.Top = '480' Tag = '0'/>
@@ -32,14 +27,12 @@
             Canvas.Left = '130' Canvas.Top = '400' Tag = '2'/>
             <Button x:Name = "Button3" Height = "75" Width = "75" Content = '3' ToolTip = "3"
             Canvas.Left = '210' Canvas.Top = '400' Tag = '3'/>
-
             <Button x:Name = "Button4" Height = "75" Width = "75" Content = '4' ToolTip = "4"
             Canvas.Left = '50' Canvas.Top = '320' Tag = '4'/>
             <Button x:Name = "Button5" Height = "75" Width = "75" Content = '5' ToolTip = "5"
             Canvas.Left = '130' Canvas.Top = '320' Tag = '5'/>
             <Button x:Name = "Button6" Height = "75" Width = "75" Content = '6' ToolTip = "6"
             Canvas.Left = '210' Canvas.Top = '320' Tag = '6'/>
-
             <Button x:Name = "Button7" Height = "75" Width = "75" Content = '7' ToolTip = "7"
             Canvas.Left = '50' Canvas.Top = '240' Tag = '7'/>
             <Button x:Name = "Button8" Height = "75" Width = "75" Content = '8' ToolTip = "8"
@@ -55,16 +48,16 @@
             Background="orange" Canvas.Left = '290' Canvas.Top = '240' Tag = ' * '/>
             <Button x:Name = "ButtonDiv" Height = "75" Width = "75" Content = '/' ToolTip = "/"
             Background="orange" Canvas.Left = '290' Canvas.Top = '320' Tag = ' / '/>
-
             <Button x:Name = "ButtonEgal" Height = "75" Width = "75" Content = '=' ToolTip = "="
             Background="yellow" Canvas.Left = '370' Canvas.Top = '480'/>
-
             <Button x:Name = "ButtonReset" Height = "75" Width = "75" Content = 'C' ToolTip = "C"
             Canvas.Left = '130' Canvas.Top = '480'/>
-
             <Button x:Name = "ButtonDot" Height = "75" Width = "75" Content = '.' ToolTip = "."
             Canvas.Left = '210' Canvas.Top = '480' Tag = '.'/>
-
+            <Button x:Name = "ButtonBracket1" Height = "75" Width = "75" Content = '(' ToolTip = "("
+            Background="orange" Canvas.Left = '50' Canvas.Top = '160' Tag = '( '/>
+            <Button x:Name = "ButtonBracket2" Height = "75" Width = "75" Content = ')' ToolTip = ")"
+            Background="orange" Canvas.Left = '130' Canvas.Top = '160' Tag = ' )'/>
         </Canvas>
 </Window>
 "@
@@ -81,15 +74,29 @@ for($i=0;$i -le 9; $i++){
 $buttonEgal = $Window.FindName("ButtonEgal")
 $buttonReset = $Window.FindName("ButtonReset")
 $textBox = $Window.FindName("TextBox")
+$historic = $Window.FindName("Historic")
 $canvas = $Window.FindName("Canvas")
 $form = $Window.FindName("Window")
+
+[string]$global:cal = ''
+$global:turn = 0
 
 
 #Connect to actions ===========================================================
 $canvas.Children | ForEach-Object {
     if($_.GetType().Name -eq "Button") {
         $_.Add_Click({param($sender, $e)
-            $textBox.text += $sender.Tag
+        if ($global:turn -eq 0) {
+                $textBox.text += $sender.Tag
+                $global:cal = $textBox.text
+            } elseif ($global:turn -lt 0) {
+                $textBox.text = $sender.Tag
+                $global:cal = $textBox.text
+                $global:turn = 0
+            } else {
+                $textBox.text += $sender.Tag
+                $global:cal += $sender.Tag
+            }
         })
     }
 }
@@ -97,16 +104,27 @@ $canvas.Children | ForEach-Object {
 #Equal Button
 $buttonEgal.Add_Click({
     try{
-        $textBox.text += " = " + (Invoke-Expression $textBox.text)
-
+        $res = Invoke-Expression $global:cal
+        #For the Error with a float
+        if ($res -eq 'Infinity') {
+             throw "Divide by 0"
+        }
+        $historic.text += $textBox.text + " = " + $res + "`n"
+        $textBox.text = $res
+        $global:cal = $res
+        $global:turn ++
     } catch {
-        $textBox.text += " = Error" 
+        $textBox.text = ''
+        $textBox.text += $global:cal + " = Error"
+        $global:turn = -1
     }
 })
 
 #Reset Button
 $buttonReset.Add_Click({
     $textBox.text = ""
+    $global:cal = ''
+    $global:turn = 0
 })
 
 Add-Type -AssemblyName System.Windows.Forms
